8e72c7e5a6b39d8b9d830e858670bffabaff5d10
Andrea Harris <Andrea@zacs-imac.lan>
2020-01-21 15:29:13 -0600

    done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 function App() {
 	const [products] = useState(data);
@@ -18,13 +19,15 @@ function App() {
 	};
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={cart}>
 		<div className="App">
-			<Navigation cart={cart} />
+			<Navigation/>
 			{/* Routes */}
 			
 			<Route exact path="/" component={Products}/>
 			<Route path="/" component={ShoppingCart}/>
 		</div>
+		</CartContext.Provider>
 		</ProductContext.Provider>
 	
 
